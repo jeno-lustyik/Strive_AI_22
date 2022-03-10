f803909fb5a005a4f09136e1b7775d9e968017d0
Jeno Lustyik <lustyik.jeno@gmail.com>
2022-03-10 18:22:52 +0100

    update

--- Chapter 02/07. Gradient Boosting and Encoding/train.py
+++ Chapter 02/07. Gradient Boosting and Encoding/train.py
@@ -6,19 +6,16 @@ from sklearn.metrics import mean_squared_error
 from sklearn.ensemble import GradientBoostingRegressor
 
 x_train, x_test, y_train, y_test = dh.get_data("./insurance.csv")
-params = {'n_estimators': 100, 'learning_rate': 0.2, 'max_depth': 2, 'random_state': 0, 'loss': 'squared_error'}
+params = {'n_estimators': 100, 'learning_rate': 0.1, 'max_depth': 3, 'random_state': 0, 'loss': 'squared_error'}
 
 model = GradientBoostingRegressor(**params)
 model.fit(x_train, y_train)
 mse = mean_squared_error(y_test, model.predict(x_test))
 
-print(mse)
-
 test_score = np.zeros((params["n_estimators"],), dtype=np.float64)
 for i, y_pred in enumerate(model.staged_predict(x_test)):
     test_score[i] = model.loss_(y_test, y_pred)
 
-print(test_score)
 fig = plt.figure(figsize=(6, 6))
 plt.subplot(1, 1, 1)
 plt.title('Deviance')
