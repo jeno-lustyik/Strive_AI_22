ae52a8631fc648a87fdf1651e2535a952539cf1c
Jeno Lustyik <lustyik.jeno@gmail.com>
2022-04-26 17:34:28 +0200

    pog

---
+++ Chapter 03/08. Transfer Learning/new.py
@@ -0,0 +1,13 @@
+import numpy as np
+import torch
+from torch import nn
+from torch import optim
+import torch.nn.functional as F
+
+
+arr = np.arange(10, 0, -1)
+
+for i, element in enumerate(arr):
+    print(i, element)
+
+
