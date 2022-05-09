217bd97045abac296bfda3794ed4945f939bf8b3
Jeno Lustyik <lustyik.jeno@gmail.com>
2022-05-09 15:39:08 +0200

    commit

--- Chapter 03/15. Chatbot/train.py
+++ Chapter 03/15. Chatbot/train.py
@@ -8,7 +8,7 @@ from torch.utils.data import Dataset, DataLoader
 from nltk_utils import bag_of_words, tokenize, stem
 from model import NeuralNet
 
-with open('intents.json', 'r') as f:
+with open('intents_jeno.json', 'r') as f:
     intents = json.load(f)
 
 all_words = []
